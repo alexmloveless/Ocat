eb15b20b9151b5ac768d8565b57c83d554af957c
Alex Loveless <alex@alexloveless.uk>
2025-06-29 22:48:42 +0100

    feat: add __main__.py for module execution support

---
+++ src/ocat/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Entry point for running Ocat as a module.
+
+This allows the package to be executed with:
+    python -m ocat [args...]
+"""
+
+from .cli import main
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
